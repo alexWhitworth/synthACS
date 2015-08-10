ca50c22cfef7d5ae3d5bb2651d37e7afb7785da5
Alex Whitworth <awhitworth@dandb.com>
2015-08-10 08:52:57 -0700

    update to categories pulled and one return name

--- 2015-08 Call_Projection/data_pull.R
+++ 2015-08 Call_Projection/data_pull.R
@@ -18,10 +18,10 @@ pull_call_data <- function(channel= "c2g", call_date= '1/01/2014') {
     and call_date >= start_date and end_date is NULL) OR (end_date is null and start_date is null)
     OR (call_date <= end_date and start_date is null  and end_date is not null))
     AND ((campaign like 'IB - 800%' and SKILL like 'IB_Organic') OR (campaign not like 'IB - 800%'))
-    then cast(calls as int) else 0 end) as mktg_call_count from sbs_cis_rpt2.dbo.CB_Daily_Call_Log_Data_Dump cd with(nolock)
+    then cast(calls as int) else 0 end) as call_count from sbs_cis_rpt2.dbo.CB_Daily_Call_Log_Data_Dump cd with(nolock)
     left join bi_sandbox.dbo.ib_ph_donotdelete ib with(nolock) on (cd.dnis = ib.tfn or cd.dnis = ib.five9did)
-    where call_date >= '", call_date, "' and Call_Type = 'Inbound' and ANI not in 
-    ('6107587400','5205842800','5205124901','8006273867')
+    where call_date >= '", call_date, "' and Call_Type = 'Inbound' and category not in ('Customer Service', 'Direct Extension') 
+    and ANI not in ('6107587400','5205842800','5205124901','8006273867')
     group by ltrim(rtrim(category)), DNIS, campaign, TFN, five9did, 
     case when (call_date >=start_date or start_date is null) then description else 'Unassigned' end , start_date, end_date,
     month(call_date), call_date order by TFN")
