775f5166c2af78b986f692ad0dc0c12c0022ec4e
U-CREDIBILITY\awhitworth <awhitworth@DC01-WIN-DWORX.Credibility.dandb.com>
2016-05-09 16:20:55 -0700

    reduce duplication in geo checks

---
+++ R/check_geo_inputs.R
@@ -0,0 +1,14 @@
+
+# @description standard checks to make sure calls to library(acs) will work
+# @param endyear An integer, indicating the latest year of the data in the survey.
+# @param span An integer in \code{c(1,3,5)} indicating the span of the desired data.
+# @param geography a valid \code{geo.set} object specifying the census geography or 
+# geographies to be fetched.
+check_geo_inputs <- function(endyear, span, geography) {
+  if (! span %in% c(1,3,5)) stop("The ACS API only supports data spans of 1, 3, and 5 years.")
+  if (endyear %% 1 != 0 | endyear < 2009) stop("endyear must be an integer >= 2009 (when ACS data begins).")
+  # other span/endyear issues handled by library(acs)
+  if (!acs::is.geo.set(geography)) stop("Supply valid geography -- class 'geo.set'.")
+  
+  # else okay; return
+}
